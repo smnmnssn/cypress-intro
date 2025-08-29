9e0acc1c9d7d3aadb51acd16206fc7928b3cff9e
Simon <simonmansson@live.se>
2025-08-29 14:46:20 +0200

    changed app/page.tsx so it shows landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
-import { db } from "@/prisma/db";
+import Landing from "./components/Landing";
 
-export default async function Home() {
-  const cookieStore = cookies();
-  const userId = cookieStore.get("auth")?.value;
-
-  if (!userId) {
-    redirect("/login");
-  }
-
-  const user = await db.user.findUnique({ where: { id: userId } });
-
-  if (!user) {
-    redirect("/login");
-  }
-
-  // Om användaren finns och är inloggad, redirecta till dashboard
-  redirect("/dashboard");
+export default function Home() {
+  return <Landing />;
 }
