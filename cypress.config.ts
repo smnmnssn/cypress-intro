a89a31b8790d744cd0d5e9241de2659cac197320
smnmnssn <simonmansson@live.se>
2025-09-03 00:03:57 +0200

    added test properties to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
+import bcrypt from "bcrypt";
 import { defineConfig } from "cypress";
 import { db } from "./prisma/db";
-import bcrypt from "bcrypt";
 
 export default defineConfig({
   e2e: {
@@ -8,20 +8,39 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       on("task", {
         async reseed() {
+          // Rensa databasen
+          await db.property.deleteMany();
           await db.user.deleteMany();
 
+          // Skapa användare
           const hashedPassword = await bcrypt.hash("hemligt", 10);
-          await db.user.create({
+          const user = await db.user.create({
             data: {
               email: "maklare@example.com",
               password: hashedPassword,
             },
           });
 
+          // Skapa test-fastigheter
+          await db.property.createMany({
+            data: [
+              {
+                address: "Testvägen 1",
+                price: 4900000,
+                status: "Såld",
+                userId: user.id,
+              },
+              {
+                address: "Testvägen 2",
+                price: 5900000,
+                status: "Ej såld",
+                userId: user.id,
+              },
+            ],
+          });
           return null;
         },
       });
-
       return config;
     },
   },
