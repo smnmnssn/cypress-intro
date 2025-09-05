2ae1a95a0b179cbc2404de8db8bff92153af0685
Simon <simonmansson@live.se>
2025-09-05 18:49:40 +0200

    minor styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import { db } from "@/prisma/db";
 import { cookies } from "next/headers";
 import Link from "next/link";
@@ -22,44 +21,38 @@ export default async function DashboardPage() {
     redirect("/login");
   }
 
-return (
-  <main className="flex flex-col items-center p-6">
-    {/* Logout */}
-    <div className="flex w-full justify-end mb-4">
-      <LogoutButton />
-    </div>
+  return (
+    <main className="flex flex-col items-center p-6">
+      {/* Logout */}
+      <div className="flex w-full justify-end mb-4">
+        <LogoutButton />
+      </div>
 
-    {/* Welcome centered */}
-    <h1 className="text-6xl font-semibold text-center mb-12">
-{      <p>Välkommen, {user.email}!</p>
-}    </h1>
+      {/* Welcome centered */}
+      <h1 className="text-6xl font-semibold text-center mb-12">
+        {<p>Välkommen, {user.email}!</p>}{" "}
+      </h1>
 
-    {/* Cards container */}
-    <div className="flex gap-6">
-      <Card className="w-65 h-80 flex items-center justify-center bg-gray-200">
-        <Link href="/clients" className="h-25 w-40 bg-green-600">
+      {/* Cards container */}
+      <div className="flex gap-6">
+        <Link href="/clients" className="h-25 w-40 bg-gray-100">
           <Button
             variant="outline"
-            className="w-full h-full text-xl font-semibold"
+            className="w-full h-full text-xl font-semibold cursor-pointer hover:bg-gray-200"
           >
             Kunder
           </Button>
         </Link>
-      </Card>
 
-      <Card className="w-65 h-80 flex items-center justify-center bg-gray-200">
-        <Link href="/properties" className="h-25 w-40 bg-blue-600">
+        <Link href="/properties" className="h-25 w-40 bg-gray-100">
           <Button
             variant="outline"
-            className="w-full h-full text-xl font-semibold"
+            className="w-full h-full text-xl font-semibold cursor-pointer hover:bg-gray-200"
           >
             Fastigheter
           </Button>
         </Link>
-      </Card>
-    </div>
-  </main>
-);
-
-
+      </div>
+    </main>
+  );
 }
