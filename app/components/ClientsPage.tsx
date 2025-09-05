a3630bae94d6ef794414de7af0dd50227864f44a
Simon <simonmansson@live.se>
2025-09-05 17:26:54 +0200

    moved header to other component

--- app/components/ClientsPage.tsx
+++ app/components/ClientsPage.tsx
@@ -5,17 +5,7 @@ import ClientList from "./ClientList";
 export default function ClientsPage() {
   return (
     <div className="p-4">
-      <ClientsHeader />
       <ClientList />
     </div>
-  );
-}
-
-function ClientsHeader() {
-  return (
-    <header className="mb-4">
-      <h1 className="text-2xl font-bold">Kundhantering</h1>
-      <p className="text-gray-600">Hantera, skapa och uppdatera kunder</p>
-    </header>
   );
 }
